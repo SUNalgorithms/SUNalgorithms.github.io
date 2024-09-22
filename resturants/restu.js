95f26f29450e907fee15a7e5e996a7c47bfacd6d
SUNalgorithms <140968755+SUNalgorithms@users.noreply.github.com>
2024-09-23 01:33:34 +0200

    Update restu.js
    back button port setting

--- resturants/restu.js
+++ resturants/restu.js
@@ -1,5 +1,5 @@
 document.getElementById("back-home").onclick = function() {
-    window.location.href = "http://127.0.0.1:5500/index.html"; 
+    window.location.href = "https://sunalgorithms.github.io/"; 
   };
   
 document.getElementById("fish").onclick = function () {
